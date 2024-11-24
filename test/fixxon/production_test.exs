ccb46dcfb56d202802a04aea4f68eabbe1e6e90d
Warren Jones <warren@pencethren.org>
2024-11-24 22:32:01 +0000

    test: today's batches functions

--- test/fixxon/production_test.exs
+++ test/fixxon/production_test.exs
@@ -17,6 +17,42 @@ defmodule Fixxon.ProductionTest do
       assert Production.list_batches() == [batch]
     end
 
+    test "list_today_batches_for_user/1 returns only batches for specified user for today" do
+      user = admin_user_fixture()
+      other_user = admin_user_fixture()
+      batch = batch_fixture(user)
+      _old_batch = old_batch_fixture(user)
+      _other_user_batch = batch_fixture(other_user)
+
+      assert Production.list_today_batches_for_user(user) == [batch]
+    end
+
+    test "list_today_totals/0 summarises production by button type and user for current date" do
+      user1 = admin_user_fixture()
+      user2 = admin_user_fixture()
+      user3 = admin_user_fixture()
+      user3_name = user3.username
+      batch_fixture(user1, %{button_type: :names, count: 1})
+      batch_fixture(user1, %{button_type: :names, count: 2})
+      batch_fixture(user1, %{button_type: :numbers, count: 4})
+      batch_fixture(user1, %{button_type: :numbers, count: 8})
+      batch_fixture(user2, %{button_type: :names, count: 16})
+      batch_fixture(user2, %{button_type: :names, count: 32})
+      batch_fixture(user2, %{button_type: :numbers, count: 64})
+      batch_fixture(user2, %{button_type: :numbers, count: 128})
+      old_batch_fixture(user1, %{button_type: :names, count: 256})
+      old_batch_fixture(user2, %{button_type: :numbers, count: 512})
+      old_batch_fixture(user3, %{button_type: :names, count: 1024})
+
+      result = Production.list_today_totals()
+
+      assert Enum.member?(result, %{username: user1.username, button_type: :names, count: 3})
+      assert Enum.member?(result, %{username: user1.username, button_type: :numbers, count: 12})
+      assert Enum.member?(result, %{username: user2.username, button_type: :names, count: 48})
+      assert Enum.member?(result, %{username: user2.username, button_type: :numbers, count: 192})
+      refute Enum.any?(result, &match?(%{username: ^user3_name}, &1))
+    end
+
     test "get_batch!/1 returns the batch with given id" do
       user = admin_user_fixture()
       batch = batch_fixture(user)
--- test/support/fixtures/production_fixtures.ex
+++ test/support/fixtures/production_fixtures.ex
@@ -13,11 +13,30 @@ defmodule Fixxon.ProductionFixtures do
       |> Enum.into(%{
         count: 42,
         order_number: "some order_number",
-        stock: true,
         button_type: :names,
         batch_type: :order
       })
       |> Fixxon.Production.create_batch(user)
+
+    batch
+  end
+
+  @doc """
+  Generate a batch from yesterday.
+  """
+  def old_batch_fixture(user = %Fixxon.Users.User{}, attrs \\ %{}) do
+    batch =
+      attrs
+      |> Enum.into(%{
+        count: 42,
+        order_number: "some order_number",
+        button_type: :names,
+        batch_type: :order,
+        user_id: user.id,
+        inserted_at: DateTime.utc_now() |> DateTime.add(-1, :day) |> DateTime.truncate(:second)
+      })
+
+    {:ok, batch} = struct(Fixxon.Production.Batch, batch) |> Fixxon.Repo.insert()
 
     batch
   end
